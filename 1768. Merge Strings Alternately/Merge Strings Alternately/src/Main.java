0e025e221539d376f990ce5947a299d05b193ef7
rafael <kunakov.rafael@gmail.com>
2023-09-21 20:43:32 +0300

    1768. Merge Strings Alternately

---
+++ 1768. Merge Strings Alternately/Merge Strings Alternately/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    private static final Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        String word1 = "abcd";
+        String word2 = "pq";
+        System.out.println(solution.mergeAlternately(word1, word2));
+    }
+}
